4bc1006c40bc86ba6b38c9b9558da929107ee820
Michal Kukla <michal.kukla91@gmail.com>
2024-03-25 12:55:20 +0100

    add basic routes structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,41 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <html lang="en">
+        <body className={inter.className}>
+          <header className="w-full bg-slate-300 p-6">
+            <ul className="flex justify-end">
+              <li className="mr-6">
+                <a
+                  className="text-blue-500 text-2xl hover:text-blue-800"
+                  href="/"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="mr-6">
+                <a
+                  className="text-blue-500 text-2xl hover:text-blue-800"
+                  href="/register"
+                >
+                  Regitster
+                </a>
+              </li>
+              <li className="mr-6">
+                <a
+                  className="text-blue-500 text-2xl hover:text-blue-800"
+                  href="/login"
+                >
+                  Login in
+                </a>
+              </li>
+            </ul>
+          </header>
+          {children}
+          <footer className="w-full bg-slate-300">Footer</footer>
+        </body>
+      </html>
+    </>
   );
 }
