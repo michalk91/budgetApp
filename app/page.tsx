53badd736b985dde933e0e4cfd90bb4839e7f6f0
Michal Kukla <michal.kukla91@gmail.com>
2024-03-30 21:00:27 +0100

    fix issue with router "/register" not working after logout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { useAppSelector } from "./redux/hooks";
+import { usePathname } from "next/navigation";
 
 export default function Home() {
+  const path = usePathname();
   const loginStatus = useAppSelector((state) => state.user.user.loginStatus);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {loginStatus === "succeeded" ? (
+      {loginStatus === "succeeded" && path !== "/register" ? (
         <p>Welcome</p>
       ) : (
         <p>You are not logged in </p>
