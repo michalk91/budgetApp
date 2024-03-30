4853f39713b478c816d00161cd0a236d477762aa
Michal Kukla <michal.kukla91@gmail.com>
2024-03-30 20:00:57 +0100

    add firestore to configuration

--- app/firebase/config.ts
+++ app/firebase/config.ts
@@ -2,6 +2,8 @@
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const API_KEY = process.env.API_KEY;
 const AUTH_DOMAIN = process.env.AUTH_DOMAIN;
@@ -26,4 +28,7 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
 export default app;
