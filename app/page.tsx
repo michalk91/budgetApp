1b5c8ccd03a8243e3a36c3deb4401d01a556cf3f
Michal Kukla <michal.kukla91@gmail.com>
2024-03-30 20:07:46 +0100

    if the user is not logged in, display information

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+"use client";
+
+import { useAppSelector } from "./redux/hooks";
 
 export default function Home() {
+  const loginStatus = useAppSelector((state) => state.user.user.loginStatus);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Welcome</p>
+      {loginStatus === "succeeded" ? (
+        <p>Welcome</p>
+      ) : (
+        <p>You are not logged in </p>
+      )}
     </main>
   );
 }
