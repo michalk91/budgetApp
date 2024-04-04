95a0202c297c37405b7f015e8d80d0997f451fe3
Michal Kukla <michal.kukla91@gmail.com>
2024-04-04 12:24:57 +0200

    create the UpdateBudget component and move the logic for updating the budget there

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -1,20 +1,11 @@
-import { SyntheticEvent, useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAppSelector, useAppDispatch } from "../redux/hooks";
-import { fetchData, updateBudget } from "../redux/usersSlice";
+import { fetchData } from "../redux/usersSlice";
+import UpdateBudget from "./UpdateBudget";
 
 export default function Dashboard() {
-  const [budgetFromInput, setBudgetFromInput] = useState("");
-
   const budgetFromStore = useAppSelector((state) => state.user.budget);
   const dispatch = useAppDispatch();
-
-  const handleUpdateBudget = (e: SyntheticEvent) => {
-    e.preventDefault();
-
-    dispatch(updateBudget(Number(budgetFromInput)));
-
-    setBudgetFromInput("");
-  };
 
   useEffect(() => {
     dispatch(fetchData());
@@ -23,27 +14,7 @@ export default function Dashboard() {
   return (
     <>
       <h2>Dashboard</h2>
-      <form className="flex m-4 items-center justify-center">
-        <p>Add budget: </p>
-
-        <input
-          type="number"
-          placeholder="0.00"
-          value={budgetFromInput}
-          className="py-3 px-2 text-md border border-blue-lighter rounded-r"
-          onChange={(e) => setBudgetFromInput(e.target.value)}
-        />
-        <div className="w-8 flex align-center bg-blue-lighter border-t border-l border-b border-blue-lighter rounded-l text-blue-dark">
-          $
-        </div>
-        <button
-          type="button"
-          onClick={handleUpdateBudget}
-          className="rounded-lg px-4 py-2 border-2 border-gray-900 text-gray-900 hover:bg-gray-900 hover:text-gray-100 duration-300"
-        >
-          confrim
-        </button>
-      </form>
+      <UpdateBudget />
       <span>Current budget: {budgetFromStore} $</span>
     </>
   );
---
+++ app/components/UpdateBudget.tsx
@@ -0,0 +1,40 @@
+import { SyntheticEvent, useState } from "react";
+import { useAppDispatch } from "../redux/hooks";
+import { updateBudget } from "../redux/usersSlice";
+
+export default function UpdateBudget() {
+  const dispatch = useAppDispatch();
+
+  const [budgetFromInput, setBudgetFromInput] = useState("");
+
+  const handleUpdateBudget = (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    dispatch(updateBudget(Number(budgetFromInput)));
+    setBudgetFromInput("");
+  };
+
+  return (
+    <form className="flex m-4 items-center justify-center">
+      <p>Add budget: </p>
+
+      <input
+        type="number"
+        placeholder="0.00"
+        value={budgetFromInput}
+        className="py-3 px-2 text-md border border-blue-lighter rounded-r"
+        onChange={(e) => setBudgetFromInput(e.target.value)}
+      />
+      <div className="w-8 flex align-center bg-blue-lighter border-t border-l border-b border-blue-lighter rounded-l text-blue-dark">
+        $
+      </div>
+      <button
+        type="button"
+        onClick={handleUpdateBudget}
+        className="rounded-lg px-4 py-2 border-2 border-gray-900 text-gray-900 hover:bg-gray-900 hover:text-gray-100 duration-300"
+      >
+        confrim
+      </button>
+    </form>
+  );
+}
