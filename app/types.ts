7a5df03662183793f1bb4114f6c2ec4fa7d4c8aa
Michal Kukla <michal.kukla91@gmail.com>
2024-04-09 15:34:44 +0200

    create file for types

---
+++ app/types.ts
@@ -0,0 +1,21 @@
+export interface Expense {
+  category: string;
+  amount: number;
+  date?: string;
+}
+
+export interface State {
+  registeredStatus: "idle" | "loading" | "succeeded" | "failed";
+  loginStatus: "idle" | "loading" | "succeeded" | "failed";
+  error: string | undefined;
+  userID: null | string;
+  username: null | string;
+  budget: number;
+  expenses: Expense[];
+}
+
+export interface User {
+  email: string;
+  password: string;
+  username?: string;
+}
