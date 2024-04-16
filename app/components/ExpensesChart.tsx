e1f5578e05ee8055f10606f0d86a0f550618d8bf
Michal Kukla <michal.kukla91@gmail.com>
2024-04-16 13:18:05 +0200

    don't show expense visualizations when there are no expenses

--- app/components/ExpensesChart.tsx
+++ app/components/ExpensesChart.tsx
@@ -34,26 +34,30 @@ export default function ExpensesChart() {
   };
 
   return (
-    <div className="w-full mt-20">
-      <span className="ml-2 font-bold text-xl mx-auto">
-        Expenses Visualization
-      </span>
-      <section className="overflow-hidden border-2 border-slate-300 rounded-lg">
-        <Chart
-          chartType="PieChart"
-          data={data}
-          options={options}
-          width={"100%"}
-          height={"400px"}
-        />
-        <Chart
-          chartType="ColumnChart"
-          data={data}
-          options={options}
-          width={"100%"}
-          height={"500px"}
-        />
-      </section>
-    </div>
+    <>
+      {expensesFromDataBase.length > 1 && (
+        <div className="w-full mt-20">
+          <span className="ml-2 font-bold text-xl mx-auto">
+            Expenses Visualization
+          </span>
+          <section className="overflow-hidden border-2 border-slate-300 rounded-lg">
+            <Chart
+              chartType="PieChart"
+              data={data}
+              options={options}
+              width={"100%"}
+              height={"400px"}
+            />
+            <Chart
+              chartType="ColumnChart"
+              data={data}
+              options={options}
+              width={"100%"}
+              height={"500px"}
+            />
+          </section>
+        </div>
+      )}
+    </>
   );
 }
