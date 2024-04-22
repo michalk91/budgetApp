06b35dd4e60283b2bc4cbd3d153902bc126c1e93
Michal Kukla <michal.kukla91@gmail.com>
2024-04-22 11:04:30 +0200

    Add a warning to prevent expenses from being deleted when setting a new budget.

--- app/setBudget/page.tsx
+++ app/setBudget/page.tsx
@@ -1,12 +1,25 @@
 "use client";
 import SetBudget from "../components/SetBudget";
 import CurrentBudget from "../components/CurrentBudget";
+import { useAppSelector } from "../redux/hooks";
+import { useState } from "react";
+import Warning from "../components/Warning";
 
 export default function Budget() {
+  const expenses = useAppSelector((state) => state.user.expenses);
+
+  const [decision, setDecision] = useState(false);
+
   return (
-    <section className="flex flex-col w-full h-screen max-h-full justify-center items-center">
-      <SetBudget />
-      <CurrentBudget />
+    <section className="flex flex-col  h-screen max-h-full justify-center items-center">
+      {(expenses?.length === 0 && !decision) || decision ? (
+        <>
+          <SetBudget />
+          <CurrentBudget />
+        </>
+      ) : (
+        <Warning setDecision={setDecision} />
+      )}
     </section>
   );
 }
--- app/types.ts
+++ app/types.ts
@@ -46,3 +46,7 @@ export interface SortOptions {
 export interface ChangeComponentsProps {
   setActive: (value: string) => void;
 }
+
+export interface WarningProps {
+  setDecision: (value: boolean) => void;
+}
