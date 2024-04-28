9a862846f0d5177f3152e009b70928ec9f6ba08f
Michal Kukla <michal.kukla91@gmail.com>
2024-04-28 17:47:17 +0200

    add author in footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Footer() {
-  return <footer className="fixed bottom-0 w-full bg-slate-300">Footer</footer>;
+  return (
+    <footer className="bottom-0 w-full bg-slate-300 text-center p-4">
+      © 2024 Michał Kukla
+    </footer>
+  );
 }
