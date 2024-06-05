6f8c49b497790e8ae14b9651a8812ba282c517fa
Michal Kukla <michal.kukla91@gmail.com>
2024-06-05 13:48:38 +0200

    center budget added date on mobile

--- app/components/BudgetAddDate.tsx
+++ app/components/BudgetAddDate.tsx
@@ -5,7 +5,7 @@ export default function BudgetAddDate({ budgetDate }: BudgetAddDateProps) {
   return (
     budgetDate && (
       <span
-        className={`text-center pl-0 p-5 md:-ml-3 text-xl max-md:-mt-6 max-md:text-lg`}
+        className={`text-center lg:pl-0 p-5 md:-ml-3 text-xl max-md:-mt-6 max-md:text-lg`}
       >
         {`(created: `}
         <b>{budgetDate}</b>
