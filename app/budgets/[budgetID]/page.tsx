4b341c055b1ac2a02e4ad9e0f8c878315820b922
Michal Kukla <michal.kukla91@gmail.com>
2024-06-16 17:11:30 +0200

    make each budget a separate page

---
+++ app/budgets/[budgetID]/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+import ShowSelectedBudget from "../../components/ShowSelectedBudget";
+import Button from "../../components/Button";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { setSelectedBudgetID } from "../../redux/budgetsSlice";
+import {
+  setSecondElemPos,
+  setAllowTransition,
+} from "@/app/redux/transitionSlice";
+import Link from "next/link";
+import { usePosition } from "@/app/hooks/usePosition";
+
+import useTransition from "@/app/hooks/useTransition";
+import { useCallback, useEffect, useState } from "react";
+import { useIDfromPathname } from "@/app/hooks/useIDfromPathname";
+
+export default function SelectedBudget() {
+  const dispatch = useAppDispatch();
+  const budgetID = useIDfromPathname();
+
+  const firstElemPos = useAppSelector((state) => state.transition.firstElemPos);
+
+  const [animate, setAnimate] = useState(false);
+
+  const onTransitionStart = useCallback(() => {
+    setAnimate(true);
+  }, []);
+
+  const onTransitionEnd = useCallback(() => {
+    setAnimate(false);
+  }, []);
+
+  const animateElemCallback = useTransition({
+    animateToElemPos: firstElemPos,
+    onTransitionStart,
+    onTransitionEnd,
+  });
+
+  const getElemPosition = usePosition();
+
+  useEffect(() => {
+    dispatch(
+      setSelectedBudgetID({
+        budgetID: budgetID,
+      })
+    );
+  }, [dispatch, budgetID]);
+
+  return (
+    <div className="flex flex-col items-center w-full mt-20 relative">
+      <div
+        ref={animateElemCallback}
+        className={`bg-white min-w-full ${
+          animate ? "" : "animate-decreaseWidth"
+        } relative text-center`}
+      >
+        <Link href="/">
+          <Button
+            handleClick={(e) => {
+              const position = getElemPosition(e.currentTarget as HTMLElement);
+
+              dispatch(setSecondElemPos(position));
+              dispatch(setAllowTransition(true));
+              dispatch(setSelectedBudgetID({ budgetID: "" }));
+            }}
+            additionalStyles="bg-red-500 hover:hover:bg-red-700 max-md:!m-2"
+          >
+            Back to budgets
+          </Button>
+        </Link>
+      </div>
+      <ShowSelectedBudget />
+    </div>
+  );
+}
