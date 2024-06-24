b5fa4d1a7912e46c301e72d92b8b016cca84540a
Michal Kukla <michal.kukla91@gmail.com>
2024-06-24 18:55:04 +0200

    change footer caption

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="shrink-0 w-full bg-slate-300 text-center p-4 mt-6">
-      © 2024 Michał Kukla
+      © 2024 Budget App, mkukla91
     </footer>
   );
 }
